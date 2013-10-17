fa93ac03632f62d30793213794b2b4a5f8676c2c
popallo <pop.popallo@gmail.com>
2013-10-17 19:10:23 +0200

    Update jquery.enlargeable.js

--- jquery.enlargeable.js
+++ jquery.enlargeable.js
@@ -18,16 +18,16 @@
  * 			<table>
  * 
  * TODO add callbacks
- * TODO make bt img as background
+ * TODO make enlargeable fullscreenable... ^^
  * TODO make the plugin more flexible
  * TODO more and more and more ...
  *
  */
 (function ( $ ) {
     var Enlargeable = function( options ) {
-    	var $element = this; //the enlargeable element
     	var $settings = $.extend({}, Enlargeable.settings, options);
-        $element.each(function(){
+        this.each(function(){
+        	var $element = $(this); //the enlargeable element
 	    	var $elementWidth = $element.width(); //element's width
 			var $elementHasMaxHeight = $element.css('max-height').length;
         	/* in case of "tooltip" option is true, test if jquery ui is in da place ! */
@@ -35,38 +35,43 @@
 	        	throw new Error('jquery.enlargeable requires jQuery ui 1.10.x OR make "tooltip" option to FALSE');
 	        }
 	        /* make the enlarge button */
-	        $element.parent().prepend('<p class="enlargeCT" title="Agrandir le tableau"><img class="enlargeBT" src="'+$settings.imgPath+'enlarge.png" /></p>');
+	        $element.parent().prepend('<div class="enlargeBT" title="Agrandir le tableau"></div>');
 	        if($settings.tooltip)
 				$('.enlargeBT').tooltip({position: { my: "center bottom-20", at: "right top" }, content: $settings.enlargeMsg });
-			$(document).on('click','.enlargeCT img',function(){
+			$('.enlargeBT',$element.parent()).on('click',function(){
 				var $this = $(this);
-				var $parent = $this.closest('.enlargeablePrison');
+				var $parent = $this.parents('.enlargeablePrison');
 				/* enlarge bt action */
-				if($this.hasClass('enlargeBT')){
+				if(!$this.hasClass('reduce')){
 					var $tooltipMsg=$settings.reduceMsg;
 					if($parent.length>0){
 						$parentWidth = $parent.width();
-						$parent.animate({width:$settings.enlargeWidth+'px'},$settings.speed);
+						$parent.each(function(){
+							$(this).animate({width:$settings.enlargeWidth+'px'},$settings.speed);
+						});
 					}
 					$element.animate({height:'100%', width:($settings.enlargeWidth)+'px'},$settings.speed);
 					if($elementHasMaxHeight>0){ //verify if element has a define max-height
 						$elementMaxHeight=$element.css('max-height');
 						$element.css('max-height','100%');
 					}
-					$this.attr('src',$settings.imgPath+'reduce.png').removeClass('enlargeBT').addClass('reduceBT');
+					
 				/* reduce bt action */
-				}else if($this.hasClass('reduceBT')){
+				}else{
 					var $tooltipMsg=$settings.enlargeMsg;
 					if($parent.length>0){
-						$parent.animate({width:$parentWidth+'px'},$settings.speed);
+						$parent.each(function(){
+							$(this).animate({width:$parentWidth+'px'},$settings.speed);
+						});
 					}
 					$element.animate({width:$elementWidth+'px'},$settings.speed);
 					if($elementHasMaxHeight>0){ //verify if element has a define max-height
 						$element.css('max-height',$elementMaxHeight);
 					}
-					$this.attr('src',$settings.imgPath+'enlarge.png').removeClass('reduceBT').addClass('enlargeBT');
+					
 					
 				}
+				$this.attr('src',$settings.imgPath+'reduce.png').toggleClass('reduce');
 				if($settings.tooltip)
 					$this.tooltip({position: { my: "center bottom-20", at: "right top" }, content: $tooltipMsg });
 			});
